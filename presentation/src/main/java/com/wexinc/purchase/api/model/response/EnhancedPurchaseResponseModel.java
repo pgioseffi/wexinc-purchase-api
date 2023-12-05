04d4e7ae5fe8c93456816bdad2aa2e455540206f
Philippe Gioseffi <pgioseffi@gmail.com>
2023-12-05 15:04:42 -0300

    Improving Swagger Opean API documentation.

--- presentation/src/main/java/com/wexinc/purchase/api/model/response/EnhancedPurchaseResponseModel.java
+++ presentation/src/main/java/com/wexinc/purchase/api/model/response/EnhancedPurchaseResponseModel.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Collection;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * Object responsible for transporting complete information about a purchase, plus its exchange rate according to the
  * country through the different layers of the application.
@@ -21,6 +23,10 @@ import java.util.Collection;
  * @since 1.0.0
  * @version 1.0.0
  */
-public record EnhancedPurchaseResponseModel(Long id, String description, LocalDateTime transactionDate,
-		BigDecimal amount, Collection<ExchangeRateDataResponseModel> exchangeRateData) {
+public record EnhancedPurchaseResponseModel(
+		@Schema(description = "Mandatory field responsible for holding the positive numeric identification of a purchase.") Long id,
+		@Schema(description = "Mandatory field responsible for holding the purchase description containing at most fifty characters.") String description,
+		@Schema(description = "Mandatory field responsible for holding the purchase transaction date. Must be in the past.") LocalDateTime transactionDate,
+		@Schema(description = "Mandatory field responsible for holding the purchase transaction amount. Must be a positive decimal number.") BigDecimal amount,
+		@Schema(description = "The exchange rate data such as country, exchange rate value itself, and effective date that the exchange rate took effect.") Collection<ExchangeRateDataResponseModel> exchangeRateData) {
 }
--- presentation/src/main/java/com/wexinc/purchase/api/model/response/ExchangeRateDataResponseModel.java
+++ presentation/src/main/java/com/wexinc/purchase/api/model/response/ExchangeRateDataResponseModel.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import com.wexinc.purchase.api.shared.constant.Country;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * Class responsible for transporting information retrieved by the american treasury exchange rate microservice through
  * the different layers of the application.
@@ -18,5 +20,8 @@ import com.wexinc.purchase.api.shared.constant.Country;
  * @version 1.0.0
  * @see com.wexinc.purchase.api.persistence.repository.PurchaseRepository PurchaseRepository
  */
-public record ExchangeRateDataResponseModel(String country, BigDecimal exchangeRate, BigDecimal convertedAmount) {
+public record ExchangeRateDataResponseModel(
+		@Schema(description = "The country object that is associated with a given exchange rate.") String country,
+		@Schema(description = "Exchange rate at which any foreign currency unit will be valued, and reported at, against the U.S. Dollar.") BigDecimal exchangeRate,
+		@Schema(description = "The purchase amount multiplied by the exchange rate.") BigDecimal convertedAmount) {
 }
