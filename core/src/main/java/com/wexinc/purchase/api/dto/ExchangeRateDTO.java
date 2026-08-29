b458ca92a9a0b63bd7444ef853fa3e1698b4eee7
Philippe Gioseffi <pgioseffi@gmail.com>
2026-08-29 14:13:09 -0300

    Javadoc correction.

--- core/src/main/java/com/wexinc/purchase/api/dto/ExchangeRateDTO.java
+++ core/src/main/java/com/wexinc/purchase/api/dto/ExchangeRateDTO.java
@@ -1,13 +1,13 @@
 package com.wexinc.purchase.api.dto;
 
-import com.wexinc.purchase.api.shared.constant.Country;
 import java.util.Collection;
 
 /**
  * Class responsible for transporting information retrieved by the American treasury exchange rate
  * microservice through the different layers of the application.
  *
- * @param data The {@link Country} object that is associated with a given exchange rate.
+ * @param data The {@link ExchangeRateDataDTO effetive data} that is associated with a given
+ *     exchange rate.
  * @author <a href="mailto:pgioseffi@gmail.com">Philippe Gioseffi &lt;pgioseffi@gmail.com&gt;</a>
  * @since 1.0.0
  * @version 1.0.0
